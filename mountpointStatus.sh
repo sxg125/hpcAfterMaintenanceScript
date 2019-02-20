ec8f203c8bf1bcefc43d7c30801f5fa61fbf5be8
Sanjaya Gajurel <sxg125@users.noreply.github.com>
2019-02-20 13:35:39 -0500

    Update mountpointStatus.sh

--- mountpointStatus.sh
+++ mountpointStatus.sh
@@ -1,37 +1,70 @@
 #!/bin/bash
 # Check the Mount Points
-# Created By Sanjaya Gajurel on May 22,2017.
+# Created By Hadrian Djohari (20180125)
+# Modified by Sanjaya Gajurel (20190219)
 
-declare -a mountpointsHead=("/mnt/projects" "/mnt/genetics" "/home" "/scratch" "/usr/local" "/mnt/pan")
-echo "The mountpoints to be monitored for head nodes are: ${mountpointsHead[@]}"
+HEADOUT="/tmp/head.lst"
+COMPOUT="/tmp/comp.lst"
+COMPOUT2="/tmp/comp2.lst"
+MISSING="/tmp/missing.lst"
+TMPLIST="/tmp/tmp.lst"
+HEADNODES="/tmp/headnodes.lst"
+COMPNODES="/tmp/computenodes.lst"
+#COMPNODES2="/etc/dsh/group/slurmrh7t"
 
-declare -a mountpointsCompute=("/mnt/projects" "/mnt/genetics" "/home" "/scratch" "/usr/local" "/mnt/pan")
-echo "The mountpoints to be monitored for compute nodes are: ${mountpointsCompute[@]}"
+#declare -a MOUNTPT=("/home" "/scratch" "/usr/local" "/mnt/pan" "/mnt/projects" "/mnt/genetics")
+#declare -a MOUNTPT2=("/mnt/rds/genetics01" "/mnt/rds/redhen01" "/mnt/rds/redhen" "/mnt/rds/rcci" "/mnt/rds/txl80")
+declare -a MOUNTPT=("/home" "/scratch" "/usr/local" "/mnt/pan" "/mnt/projects" "/mnt/genetics" "/mnt/rds/genetics01" "/mnt/rds/redhen" "/mnt/rds/rcci" "/mnt/rds/txl80" "/mnt/rds/ski")
 
-
-echo "Check the following nodes for Missing Mount Points ...."
-
-filename="/tmp/nodewoscratch.lst"
+# echo "The mountpoints to be monitored are: ${MOUNTPT[@]}" > $MISSING
+echo > $TMPLIST
+# echo > $MISSING  #Empty File check fails with echo
 
 # For Head Nodes
-for i in "${mountpointsHead[@]}"
-do
-  /opt/xcat/bin/xdsh <list-of-servers> "df -hP | grep $i | wc -l" | grep ".*: 0" | cut -f1 -d':' > $filename 2> /tmp/errfile
+/usr/local/bin/pdsh -g headrh7 -t 120 hostname 2>/dev/null | cut -f1 -d":" > $HEADNODES
+/usr/local/bin/pdsh -g headrh7 -t 120 "df -hP" > $HEADOUT
 
-if [ -s $filename ]
-then
-  cat  $filename
-fi
+# Go through the each mount point and check if it is missing
+# Then devise a way to mount it or complain loudly via email
+
+for i in "${MOUNTPT[@]}"
+do
+  for j in `cat $HEADNODES`
+  do
+    echo "$j $i: `cat $HEADOUT | grep $j | grep $i | wc -l`" >> $TMPLIST
+  done
 done
 
-# For Compute Nodes
-for i in "${mountpointsCompute[@]}"
-do
-  /opt/xcat/bin/xdsh <compute-node-group> "df -hP | grep $i | wc -l" | grep ".*: 0" | cut -f1 -d':' > $filename 2> /tmp/errfile
-#  /opt/xcat/bin/xdsh comp100 "pan_df -hP | grep $i | wc -l" | grep ".*: 0" | cut -f1 -d':' > $filename 2> /tmp/errfile
+# For compute nodes
+#/usr/local/bin/pdsh -w comp[032-055]t,comp[057-065]t,comp[067-252]t,gpu[005-038]t,smp[02-07]t -t 30 -u 30 hostname 2>/dev/null | cut -f1 -d":" > $COMPNODES
+/usr/local/bin/pdsh -g slurmrh7t -t 120 hostname 2>/dev/null | cut -f1 -d":" > $COMPNODES
+/usr/local/bin/pdsh -g slurmrh7t -t 120 "df -hP" > $COMPOUT
 
-if [ -s $filename ]
-then
-  cat  $filename
-fi
+# Go through the each mount point and check if it is missing
+# Then devise a way to mount it or complain loudly via email
+
+for i in "${MOUNTPT[@]}"
+do
+  for j in `cat $COMPNODES`
+  do
+    echo "$j $i: `cat $COMPOUT | grep $j | grep $i | wc -l`" >> $TMPLIST
+  done
 done
+
+cat $TMPLIST | grep ".*: 0" | cut -f1 -d':' | sort > $MISSING
+
+# Set to remount (only for non-panasas)
+
+if [ -s ${MISSING} ]
+then
+# Mail to the admin
+cat $MISSING | mail -s "Mounting points missing (if any) & fixed" sxg125@case.edu
+
+# Mount the mountpoints
+  for node in `cat $MISSING | cut -f1 -d' ' | uniq | tail -n+2`
+   do
+    ssh $node /xcatpost/rh7remount
+    ssh $node mount $mtpt
+   done
+fi
+
