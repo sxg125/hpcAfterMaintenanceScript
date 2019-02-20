1db0f7e4c544c80a811c943e173455408459d589
Sanjaya Gajurel <sxg125@users.noreply.github.com>
2019-02-20 13:41:46 -0500

    Update licenseStatus.sh

--- licenseStatus.sh
+++ licenseStatus.sh
@@ -5,11 +5,26 @@
 declare -a ports=("<port#>@<server>" "<port#>@<server>" "<port#>@<server>" "<port#>@<server>")
 echo "Licenses for Schrodinger, Matlab MDCS, Matlab PCT to be monitired ....."
 
+echo "Licenses for Schrodinger, Matlab MDCS, Matlab PCT, ANSYS, PGI,INTEL & COMSOL to be monitired ....." > $MSG
+
 for i in ${ports[@]}
 do
- ssh <master-node> /usr/local/matlab/R2015b/etc/lmstat -a -c $i | grep UP | sed -n 2p
+ ssh <license-server> <path-to-lmstat>/lmstat -a -c $i | grep UP | sed -n 2p | wc -l | grep 0 > $filename 2> /tmp/errfile
+ if [ -s $filename ]
+then
+  ssh <license-server> <path-to-lmstat>/lmstat -a -c $i | grep UP | sed -n 2p >> $MSG
+  mail -s "Check the Software Licenses" <email-address> < $MSG
+fi
+
 done
 
 # for Mathematica
-echo "Checking Licenses for Mathematica ...."
-ssh <master-node> /usr/local/mathematica/MathLM/monitorlm -format text | grep "MathLM Version"
+echo "Checking Licenses for Mathematica ...." >> $MSG
+ssh <license-server> <path-to-monitorlm>/monitorlm -format text | grep "MathLM Version" | wc -l | grep 0 > $filename 2> /tmp/errfile
+if [ -s $filename ]
+then
+  ssh<license-server> <path-to-monitorlm>/monitorlm -format text | grep "MathLM Version" >> $MSG
+  echo "Trying to Re-start Mathlm" >> $MSG
+  systemctl restart mathlm
+  mail -s "Check Mathematica  Licenses" <email-address> < $MSG
+fi
